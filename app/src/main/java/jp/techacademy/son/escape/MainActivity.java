5110acf2d669237f8702cb6e300c9cb2ec278f23
sonyanai <taison0930@outlook.jp>
2018-02-25 19:22:26 +0900

    version変更 リストの表示について 改行した時つまってるから  textViewを横に並べて表示するようにする
     version 1.02
     リスト修正new_list,adapter
     editTextアンダーライン削除
    見やすさ改善
    
    version 1.03
    ちょいと修正
    
    version 1.04
    詳細画面でリスト切り替えできないようにした
    
    version1.05
    字数制限の排除
    
    version1.06
    広告付けてるなう
    josnファイル削除するためにandroid studioを閉じる
    
    とりあえず広告はOK
    ログインした時の処理とリクエストコード

--- app/src/main/java/jp/techacademy/son/escape/MainActivity.java
+++ app/src/main/java/jp/techacademy/son/escape/MainActivity.java
@@ -1,13 +1,8 @@
 package jp.techacademy.son.escape;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,8 +15,6 @@ import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int PERMISSIONS_REQUEST_CODE = 100;
 
 
 
@@ -44,22 +37,13 @@ public class MainActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
-
-
-
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user!=null){
-            watchFragment fragmentWatch = new watchFragment();
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            // 他にも、メソッドにはreplace removeがあります
-            // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
-            transaction.add(R.id.container, fragmentWatch);
-            // 最後にcommitを使用することで変更を反映します
-            transaction.commit();
-        }else{
-            permission();
-        }
-
+        watchFragment fragmentWatch = new watchFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        // 他にも、メソッドにはreplace removeがあります
+        // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
+        transaction.add(R.id.container, fragmentWatch);
+        // 最後にcommitを使用することで変更を反映します
+        transaction.commit();
 
 
     }
@@ -75,25 +59,16 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.addButton:
                 user = FirebaseAuth.getInstance().getCurrentUser();
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
-                        // 許可されている
-                        if(user==null) {
-                            intentLogin();
-                        }else{
-                            sendFragment fragmentSend = new sendFragment();
-                            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                            // 他にも、メソッドにはreplace removeがあります
-                            // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
-                            transaction.replace(R.id.container, fragmentSend);
-                            // 最後にcommitを使用することで変更を反映します
-                            transaction.commit();
-                        }
-                    } else {
-                        // 許可されていないので許可ダイアログを表示する
-                        requestPermissions(new String[]{Manifest.permission.INTERNET}, PERMISSIONS_REQUEST_CODE);
-                    }
+                if(user==null) {
+                    intentLogin();
+                }else{
+                    sendFragment fragmentSend = new sendFragment();
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    // 他にも、メソッドにはreplace removeがあります
+                    // メソッドの1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
+                    transaction.replace(R.id.container, fragmentSend);
+                    // 最後にcommitを使用することで変更を反映します
+                    transaction.commit();
                 }
         }
         return false;
@@ -104,66 +79,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void permission(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
-                // 許可されている
-                //Log.d("ANDROID", "許可されている");
-            } else {
-                //Log.d("ANDROID", "許可されていない");
-                // 許可されていないので許可ダイアログを表示する
-                requestPermissions(new String[]{Manifest.permission.INTERNET}, PERMISSIONS_REQUEST_CODE);
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_CODE:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //許可された
-                    intentLogin();
-                } else {
-                    //許可されなかった
-                    dialog();
-
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void dialog(){
-        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setTitle("");
-        alertDialogBuilder.setMessage("インターネットへのアクセスを許可してください");
-
-        // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
-        alertDialogBuilder.setPositiveButton("OK",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //Log.d("UI_PARTS", "肯定ボタン");
-                        permission();
-                    }
-                });
-        // 否定ボタンに表示される文字列、押したときのリスナーを設定する
-        alertDialogBuilder.setNegativeButton("NO",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //Log.d("UI_PARTS", "否定ボタン");
-                    }
-                });
-
-
-        // AlertDialogを作成して表示する
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
-    }
 
 }
 
